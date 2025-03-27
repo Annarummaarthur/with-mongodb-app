fad551c23e117fc792fceb59caa421dbf70ad53c
Annarummaarthur <arthur.annarumma@gmail.com>
2025-03-27 10:52:03 +0100

    Update route.ts

--- app/api/movies/[idMovie]/comments/route.ts
+++ app/api/movies/[idMovie]/comments/route.ts
@@ -8,19 +8,19 @@ export async function GET(request: Request, { params }: any): Promise<NextRespon
   try {
     const client: MongoClient = await clientPromise;
     const db: Db = client.db('sample_mflix');
-
+    
     const { idMovie } = params;
     if (!ObjectId.isValid(idMovie)) {
       return NextResponse.json({ status: 400, message: 'Invalid movie ID', error: 'ID format is incorrect' });
     }
-
-    const comments = await db.collection('comments').find({ movie_id: new ObjectId(idMovie) }).toArray();
-
-    if (!comments.length) {
-      return NextResponse.json({ status: 404, message: 'No comments found', error: 'No comments found for the given movie ID' });
+    
+    const movie = await db.collection('comments').findOne({ _id: new ObjectId(idMovie) });
+    
+    if (!movie) {
+      return NextResponse.json({ status: 404, message: 'Movie not found', error: 'No movie found with the given ID' });
     }
-
-    return NextResponse.json({ status: 200, data: { comments } });
+    
+    return NextResponse.json({ status: 200, data: { movie } });
   } catch (error: any) {
     return NextResponse.json({ status: 500, message: 'Internal Server Error', error: error.message });
   }
