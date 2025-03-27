a4155064601dc50f1989c110ecda35861beb7e8f
Annarummaarthur <arthur.annarumma@gmail.com>
2025-03-27 11:11:08 +0100

    Update route.ts

--- app/api/movies/[idMovie]/comments/route.ts
+++ app/api/movies/[idMovie]/comments/route.ts
@@ -14,13 +14,13 @@ export async function GET(request: Request, { params }: any): Promise<NextRespon
       return NextResponse.json({ status: 400, message: 'Invalid movie ID', error: 'ID format is incorrect' });
     }
     
-    const movie = await db.collection('movies').findOne({ _id: new ObjectId(idMovie) });
+    const comments = await db.collection('comments').findOne({ _id: new ObjectId(idMovie) });
     
-    if (!movie) {
+    if (!comments) {
       return NextResponse.json({ status: 404, message: 'Movie not found', error: 'No movie found with the given ID' });
     }
     
-    return NextResponse.json({ status: 200, data: { movie } });
+    return NextResponse.json({ status: 200, data: { comments } });
   } catch (error: any) {
     return NextResponse.json({ status: 500, message: 'Internal Server Error', error: error.message });
   }
